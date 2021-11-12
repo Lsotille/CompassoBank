fb22a75d7e49e1adaee5ab217a31904a73e25401
LucasSotille <60549310+LucasSotille@users.noreply.github.com>
2021-11-12 11:06:58 -0300

    Initial commit

---
+++ src/main/java/com/example/compassobank/CompassoBankApplication.java
@@ -0,0 +1,13 @@
+package com.example.compassobank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompassoBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompassoBankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/compassobank/CompassoBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.compassobank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompassoBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
