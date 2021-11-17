732fe9d84831ffc68366f1a9a6df78e7d2e71fed
LucasSotille <60549310+LucasSotille@users.noreply.github.com>
2021-11-17 10:37:01 -0300

    CB-02-AssociadoEntity, criacao da entity do associado com seus atributos junto com DTO E DTOForm

---
+++ src/main/java/com/example/compassobank/dto/AssociadoFormDTO.java
@@ -0,0 +1,23 @@
+package com.example.compassobank.dto;
+
+import lombok.Data;
+
+
+
+
+@Data
+public class AssociadoFormDTO {
+
+    private Long id;
+
+    private String nome;
+
+    private String trabalho;
+
+    private Integer idade;
+
+    private Integer cpf;
+
+    private Integer cnpj;
+
+}
