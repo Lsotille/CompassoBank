53377b35b353b28b5fa9bf5cb209c3aacc6a5a08
LucasSotille <60549310+LucasSotille@users.noreply.github.com>
2021-11-19 12:00:18 -0300

    CB-013-AdicaodoSpringFox as dependecias do mavem para fazer a documentacao do projeto de forma automatica

---
+++ src/test/java/com/example/compassobank/services/AgenciaServiceTest.java
@@ -0,0 +1,17 @@
+package com.example.compassobank.services;
+
+import com.example.compassobank.entity.Agencia;
+import org.junit.jupiter.api.Test;
+
+public class AgenciaServiceTest {
+
+
+@Test
+public void cadastraTest(){
+    Agencia agencia = new Agencia();
+
+}
+
+
+
+}
