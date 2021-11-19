73164da041f6e6c5368737167dbcede0b1a90e40
Eduardo K <dudukuiava6@gmail.com>
2021-11-19 16:27:42 -0300

    CB-14-EntityContaEmpresarial Foi criado a entidade ContaEmpresarial e sua DTO

---
+++ src/main/java/com/example/compassobank/dto/ContaEmpresarialFormDTO.java
@@ -0,0 +1,25 @@
+package com.example.compassobank.dto;
+
+import lombok.Data;
+
+@Data
+public class ContaEmpresarialFormDTO {
+
+    private String agencia;
+
+    private double saldo;
+
+    private String senha;
+
+    private double juros;
+
+    private String nomeFantasia;
+
+    private String razaoSocial;
+
+    private String tipoEmpresa;
+
+    private double limite;
+
+    private double credito;
+}
