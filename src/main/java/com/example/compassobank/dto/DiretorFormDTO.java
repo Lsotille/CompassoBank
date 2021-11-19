86a38121a717a8473e5429a08cc80554a973e656
BrunoCompasso <bruno.potrich_BOLS@compasso.com.br>
2021-11-19 10:17:50 -0300

    CB-10-DIRETOR ENTITY ENTITY E RESPECTIOS DTOS CRIADOS

---
+++ src/main/java/com/example/compassobank/dto/DiretorFormDTO.java
@@ -0,0 +1,16 @@
+package com.example.compassobank.dto;
+
+import lombok.Data;
+
+@Data
+public class DiretorFormDTO {
+
+    private Long agencia;
+
+    private String cargo;
+
+    private double salario;
+
+    private String regiaoVigencia;
+
+}
